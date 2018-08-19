21a277fe9e24566acbe99edbad387636ba6cd58b
akaadi <akaadi.now@outlook.com>
2018-08-19 12:18:41 +0530

    AreaCalculator.py created.

---
+++ AreaCalculator.py
@@ -0,0 +1,20 @@
+# create a calculator that can compute the area of the shapes like circle or triangle etc.
+
+print("the program to calculate areas is running")
+
+# taking the input from the user
+option = input("Please enter C for Circle or T for Triangle: ")
+
+# checking for inputs and returning/printing the corresponding areas
+if option == 'C' or option == 'c':
+    r = input("Enter the radius:")
+    area_circle = 3.14 * float(r*r)
+    print(area_circle)
+elif option == 'T':
+    b = input("Enter the base:")
+    h = input("Enter the height:")
+    area_triangle = 0.5 * float(b*h)
+    print(area_triangle)
+else:
+    print("Empty")
+    
